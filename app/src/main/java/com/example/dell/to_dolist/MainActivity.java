5187d8caa51fe56647c325e14484fd4ae503d070
Shubham Narang <shubham12104655@gmail.com>
2018-10-30 09:57:32 -0300

    Update MainActivity.java to include pop window button and oncreate

--- app/src/main/java/com/example/dell/to_dolist/MainActivity.java
+++ app/src/main/java/com/example/dell/to_dolist/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.dell.to_dolist;
 
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,10 +18,22 @@ public class MainActivity extends AppCompatActivity {
 
     private AppDatabase mDb;
     private TextView txtView;
+    private Button add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        add = (Button) findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Pop.class));
+
+
+            }
+        });
+
+
         mDb = AppDatabase.getInMemoryDatabase(getApplicationContext());
 //        TextView txtView=findViewById(R.id.txtView);
         populateDb();
