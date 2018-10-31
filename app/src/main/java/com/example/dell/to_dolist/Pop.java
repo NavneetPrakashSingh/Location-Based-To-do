00ec63f446fc342abb51a9289e532fee8953df92
Shubham Narang <shubham12104655@gmail.com>
2018-10-30 23:22:46 -0300

    added sublist menu functionality .....plus finalized calendar functionality

--- app/src/main/java/com/example/dell/to_dolist/Pop.java
+++ app/src/main/java/com/example/dell/to_dolist/Pop.java
@@ -2,33 +2,64 @@ package com.example.dell.to_dolist;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.DisplayMetrics;
-import android.view.View;
-import android.widget.DatePicker;
-import android.widget.TextView;
 
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.app.AlertDialog;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class Pop extends Activity {
 
     //private static final String TAG = "Pop";
 
-    private TextView mDisplayDate;
+
     private DatePickerDialog.OnDateSetListener mDateSetListener;
+
+    private EditText editTxt;
+    private ImageButton btn;
+    private ImageButton submit;
+    private ImageButton location;
+    private ImageButton calender;
+    private ListView list;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> arrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop);
 
-        mDisplayDate = (TextView)findViewById(R.id.mdisplay);
+
+        editTxt = (EditText) findViewById(R.id.editText);
+        btn = (ImageButton) findViewById(R.id.button);
+        submit = (ImageButton) findViewById(R.id.submit);
+        location = (ImageButton) findViewById(R.id.location);
+        calender = (ImageButton) findViewById(R.id.calender);
+        list = (ListView) findViewById(R.id.listView);
+        arrayList = new ArrayList<String>();
+
+
+        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, arrayList);
+
+        list.setAdapter(adapter);
 
         DisplayMetrics metric = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metric);
@@ -36,11 +67,45 @@ public class Pop extends Activity {
         int width = metric.widthPixels;
         int height = metric.heightPixels;
 
-        getWindow().setLayout((int)(width*0.8),(int)(height*0.8));
+        getWindow().setLayout((int) (width * 0.8), (int) (height * 0.8));
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // View view1 = (LayoutInflater.from(Pop.this)).inflate(R.layout.user_input, null);
+
+                AlertDialog.Builder builder2 = new AlertDialog.Builder(Pop.this);
+                LayoutInflater inflater = Pop.this.getLayoutInflater();
+                ViewGroup parent = null;
+                View view1 = inflater.inflate(R.layout.user_input, parent , false);
+                final EditText userInput1 = (EditText) view1.findViewById(R.id.userinput);
+                builder2.setView(view1);
+
+                builder2.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        String tag = userInput1.getText().toString();
+                       // this line adds the data of your EditText and puts in your array
+                        arrayList.add(tag);
+                        // next thing you have to do is check if your adapter has changed
+                        adapter.notifyDataSetChanged();
+                    }
+                })
+                        .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        })
+                        .show();
 
 
+            }
+        });
 
-        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+
+        calender.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Calendar cal = Calendar.getInstance();
@@ -52,12 +117,11 @@ public class Pop extends Activity {
                         Pop.this,
                         android.R.style.Theme_Holo_Light_Dialog_MinWidth,
                         mDateSetListener,
-                        year,month,day);
+                        year, month, day);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
             }
         });
-
 
 
         mDateSetListener = new DatePickerDialog.OnDateSetListener() {
@@ -65,14 +129,16 @@ public class Pop extends Activity {
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                 month = month + 1;
                 String date = month + "/" + day + "/" + year;
-                mDisplayDate.setText(date);
+               // mDisplayDate.setText(date);
             }
         };
 
 
-
-
-
     }
-
 }
+
+
+
+
+
+
